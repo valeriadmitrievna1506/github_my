8719cf5491ebfc6ef4381a3804f1ad9a323c2579
valeriadmitrievna1506 <63322690+valeriadmitrievna1506@users.noreply.github.com>
2020-04-09 23:53:21 +0300

    OMYGOD
    some changes...

---
+++ New Unity Project/Assets/MenuControll.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControll : MonoBehaviour
+{
+    public Animator[] anim = new Animator[4];           //0 - play
+    public GameObject MainMenu;                         //1 - options
+    public GameObject OptionsTitle;                     //2 - quit
+                                                        //3 - baack
+
+
+    ///----------PLAY----------//
+    public void PlayPressed()
+    {
+        anim[0].SetBool("pressed", true);
+        StartCoroutine(play());
+    }
+    IEnumerator play()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Debug.Log("PLAY!");
+        //SceneManager.LoadScene("Game1");
+    }
+
+    //----------OPTIONS----------//
+    public void OptionsPressed()
+    {
+        anim[1].SetBool("pressed", true);
+        StartCoroutine(options());
+    }
+    IEnumerator options()
+    {
+        yield return new WaitForSeconds(0.3f);
+        MainMenu.SetActive(false);
+        OptionsTitle.SetActive(true);
+    }
+
+    //----------BACK----------//
+    public void BackPressed()
+    {
+        anim[3].SetBool("pressed", true);
+        StartCoroutine(back());
+    }
+    IEnumerator back()
+    {
+        yield return new WaitForSeconds(0.3f);
+        MainMenu.SetActive(true);
+        OptionsTitle.SetActive(false);
+    }
+
+    //----------QUIT----------//
+    public void QuitPressed()
+    {
+        anim[1].SetBool("pressed", true);
+        StartCoroutine(quit());
+    }
+    IEnumerator quit()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Debug.Log("QUIT!");
+        //Application.Quit();
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/hero_control.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero_control : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    private float moveX;
+    private bool isRight = true;
+
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    private bool isGrounded;
+    public float groundRadius = 0.4f;
+
+    //public GameObject resp, cam;
+    //int N = 5;
+    //public GameObject[] lives = new GameObject[5];
+    //public GameObject dead_screen;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        animator.SetBool("grounded", isGrounded);
+        animator.SetFloat("ySpeed", rb.velocity.y);
+        moveX = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("xSpeed", Mathf.Abs(moveX));
+        flip();
+        rb.velocity = new Vector2(moveX * 8, rb.velocity.y);
+
+    }
+
+    void Update()
+    {
+
+        if (isGrounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            animator.SetBool("grounded", false);
+            rb.AddForce(new Vector2(0, 1000));
+        }
+    }
+
+    void flip()
+    {
+        if ((isRight && moveX < 0) || (!isRight && moveX > 0))
+        {
+            transform.localScale = new Vector3(transform.localScale.x * (-1), transform.localScale.y, transform.localScale.z);
+            isRight = !isRight;
+        }
+    }
+
+
+
+
+}

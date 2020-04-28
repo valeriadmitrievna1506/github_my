5447e62139b9bd4c9f49040e024f574579304471
valeriadmitrievna1506 <63322690+valeriadmitrievna1506@users.noreply.github.com>
2020-04-28 17:51:00 +0300

    с кривыми переходами

---
+++ New Unity Project/Assets/levelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public void LoadScene()
+    {
+        StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator loadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
